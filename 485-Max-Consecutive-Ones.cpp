a10fe9188fe35d8866f8f255a390286b3f856863
Anish Kumar <anishjangra3722@gmail.com>
2024-10-09 14:50:50 +0530

    [485] [Time Beats: 58.42%] [Memory Beats: 17.44%] - LeetPush

--- 485-Max-Consecutive-Ones.cpp
+++ 485-Max-Consecutive-Ones.cpp
@@ -2,30 +2,28 @@ class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
 
+        int low = -1;
+        int max = 0;
+
         nums.push_back(0);
         int len = nums.size();
-        vector<int> v;
-
-        int max = 0;
 
         for(int i = 0 ; i < len ; i++){
 
+            if(nums[i] == 0){
+                if((i - low -1)  > max){
+                    max = i-low -1 ;
+                    low = i;
+                }
 
-            if(nums[i] == 1){
-                max+= 1;
+                else low = i;
             }
-            else{
-                v.push_back(max);
-                max = 0;
-            }
+
+
         }
 
-        int high = 0;
+        return max;
 
-        for(auto it : v){
-            if(it > high) high = it;
-        }
-        return high;
-        
+
     }
 };
