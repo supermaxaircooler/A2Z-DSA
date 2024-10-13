7a0cd717a7bdc6e07be46e779ec550b8e8f673c4
Anish Kumar <anishjangra3722@gmail.com>
2024-10-14 01:01:01 +0530

    Update 31-Next-Permutation.cpp

--- 31-Next-Permutation.cpp
+++ 31-Next-Permutation.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    int binarysearch(vector<int>& nums , int start , int end , int target){
+    int binarysearch(vector<int>& nums , int start , int end , int target){   // a function to find appropriate place for an element that means a[i-1] <= a[i] and a[i+1] > a[i] and also i am a genius
         
         if(end <= start && target < nums[start]) return start ;
         if(end <= start && target >= nums[start]) return start+1 ;
@@ -25,10 +25,10 @@ public:
         int len = nums.size();
 
         if(len == 1) return;
-        if(len ==2){
-            swap(nums[0], nums[1]);
-            return;
-        }
+        // if(len ==2){
+        //     swap(nums[0], nums[1]);
+        //     return;
+        // }
 
 
         if(nums[len-1] > nums[len-2]){
@@ -38,7 +38,8 @@ public:
             int k = len-1;
             bool complete = false;
 
-            while(nums[k-1] >= nums[k] ){
+            while(nums[k-1] >= nums[k] ){                // if there a descending order following an element then that element is swapped with its immediate 
+                                                        //  greater element and the descending order part of array is sorted
                 k--;
                 if(k ==0){
                     complete = true;
