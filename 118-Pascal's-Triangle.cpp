e03c2a435a73a683f81bc2b798b5239f5ff7657e
Anish Kumar <anishjangra3722@gmail.com>
2024-10-16 18:26:28 +0530

    [118] [Time Beats: 34.07%] [Memory Beats: 91.22%] - LeetPush

---
+++ 118-Pascal's-Triangle.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+
+
+        vector<vector<int>> pascalt ;
+
+
+
+        pascalt.push_back({1});
+
+        for(int i = 1 ; i< numRows ; i++){
+            vector<int> temp;
+
+            temp.push_back(1);
+            for(int j = 1 ; j < pascalt.back().size() ; j++){
+                temp.push_back(pascalt.back()[j] + pascalt.back()[j-1]);
+
+            }
+            temp.push_back(1);
+
+            pascalt.push_back(temp);
+
+
+        }
+
+
+        return pascalt;
+        
+    }
+};
