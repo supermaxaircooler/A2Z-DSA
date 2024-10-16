132554d4b397bf8db3c34c8876ab9eaca1deb7c5
Anish Kumar <anishjangra3722@gmail.com>
2024-10-16 19:36:42 +0530

    Update 118-Pascal's-Triangle.cpp

--- 118-Pascal's-Triangle.cpp
+++ 118-Pascal's-Triangle.cpp
@@ -4,26 +4,28 @@ public:
 
 
         vector<vector<int>> pascalt ;
+        pascalt.push_back({1});
+
+        for(int i = 1 ; i< numRows ; i++){
+            vector<int> temp;
+
+            temp.push_back(1);
+            for(int j = 1 ; j < pascalt.back().size() ; j++){
+                temp.push_back(pascalt.back()[j] + pascalt.back()[j-1]);
+            }
+            temp.push_back(1);
+
+            pascalt.push_back(temp);
+            return pascalt;
+        }
 
 
-
-        // pascalt.push_back({1});
-
-
-        // for(int i = 1 ; i< numRows ; i++){
-        //     vector<int> temp;
-
-        //     temp.push_back(1);
-        //     for(int j = 1 ; j < pascalt.back().size() ; j++){
-        //         temp.push_back(pascalt.back()[j] + pascalt.back()[j-1]);
-
-        //     }
-        //     temp.push_back(1);
-
-        //     pascalt.push_back(temp);
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
 
 
-        // }
+        vector<vector<int>> pascalt ;
 
         for(int i = 1 ; i <= numRows ; i++){
 
@@ -37,7 +39,7 @@ public:
                 temp.push_back(x);
 
             }
-            // temp.push_back(1);
+
 
             pascalt.push_back(temp);
 
