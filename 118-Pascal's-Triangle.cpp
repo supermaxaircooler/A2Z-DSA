b83e745dd9d76902c4c1e9d57f057d5c6bfda8cc
Anish Kumar <anishjangra3722@gmail.com>
2024-10-16 19:35:17 +0530

    [118] [Time Beats: 34.07%] [Memory Beats: 39.84%] - LeetPush

--- 118-Pascal's-Triangle.cpp
+++ 118-Pascal's-Triangle.cpp
@@ -7,17 +7,37 @@ public:
 
 
 
-        pascalt.push_back({1});
+        // pascalt.push_back({1});
 
-        for(int i = 1 ; i< numRows ; i++){
+
+        // for(int i = 1 ; i< numRows ; i++){
+        //     vector<int> temp;
+
+        //     temp.push_back(1);
+        //     for(int j = 1 ; j < pascalt.back().size() ; j++){
+        //         temp.push_back(pascalt.back()[j] + pascalt.back()[j-1]);
+
+        //     }
+        //     temp.push_back(1);
+
+        //     pascalt.push_back(temp);
+
+
+        // }
+
+        for(int i = 1 ; i <= numRows ; i++){
+
             vector<int> temp;
-
             temp.push_back(1);
-            for(int j = 1 ; j < pascalt.back().size() ; j++){
-                temp.push_back(pascalt.back()[j] + pascalt.back()[j-1]);
+
+            int x= 1;
+
+            for(int j = 1; j < i ; j++){
+                x = (x*(i-j))/j;
+                temp.push_back(x);
 
             }
-            temp.push_back(1);
+            // temp.push_back(1);
 
             pascalt.push_back(temp);
 
